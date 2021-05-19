c6859cec512d6258045a28c49b77d6c616984fa6
battlefishisfighting <1072351119@qq.com>
2021-05-19 17:23:15 +0800

    animeChange

--- Assets/Scripts/Anime/ImageController.cs
+++ Assets/Scripts/Anime/ImageController.cs
@@ -7,8 +7,13 @@ public class ImageController : MonoBehaviour
 {
     [SerializeField, Tooltip("正向播放速度")] private float forwardSpeed = 1;
     [SerializeField, Tooltip("反向播放速度")] private float reverseSpeed = -1;
-    
+    [SerializeField, Tooltip("动画是否延迟")] private bool WhetherDelay = false;//逻辑反了。。。
+    [SerializeField,Tooltip("延迟时间")] private float Delaytime;
+
     private Animator animator;
+    private AnimatorStateInfo stateInfo;
+
+    private float t = 0;
 
     private void Start()
     {
@@ -17,6 +22,10 @@ public class ImageController : MonoBehaviour
 
     private void Update()
     {
+        animator.SetBool("WhetherDelay", WhetherDelay);
+
+        PlayDelay();
+
         if (PortConnectController.Instance.DigitalSignal)
         {
             animator.SetBool("Start", true);
@@ -28,5 +37,32 @@ public class ImageController : MonoBehaviour
             animator.SetBool("Start", false);
         }
         animator.SetBool("Sit", PortConnectController.Instance.DigitalSignal);
+    }
+
+    //播放延迟
+    private void PlayDelay()
+    {
+        stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if(stateInfo.IsName("待机动画") && WhetherDelay && PortConnectController.Instance.DigitalSignal)
+        {
+            t += Time.deltaTime;
+            if(t >= Delaytime)
+            {
+                t = 0;
+                WhetherDelay = false;
+            }
+        }
+    }
+
+    //动画事件
+    private void AnimeStartSetTrue()
+    {
+        WhetherDelay = false;
+    }
+
+    //动画事件
+    private void AnimeStartSetFalse()
+    {
+        WhetherDelay = true;
     }
 }
